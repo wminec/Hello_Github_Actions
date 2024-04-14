f9ac084c18f6b7b7be44afbc251b809aba3febe9
wminec <wminec@gmail.com>
2024-04-14 19:38:45 +0900

    zxc

--- Hello_Actions_with_Terraform_Azure/main.tf
+++ Hello_Actions_with_Terraform_Azure/main.tf
@@ -11,9 +11,10 @@ terraform {
 
     backend "azurerm" {
         resource_group_name     = "tfstate"
-        storage_account_name    = "tfstatesye"
+        storage_account_name    = "tfstateaction"
         container_name          = "tfstate"
         key                     = "terraform.tfstate"
+        use_oidc                = "true"
     }
 }
 
