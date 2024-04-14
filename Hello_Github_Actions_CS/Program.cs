2532f1bea7b80d73a833a7ad15c024776a12193b
wminec <wminec@gmail.com>
2024-04-14 13:48:20 +0900

    aa

--- Hello_Github_Actions_CS/Program.cs
+++ Hello_Github_Actions_CS/Program.cs
@@ -3,10 +3,10 @@
     static void Main(string[] args)
     {
         #if DEBUG
-            Console.WriteLine("Hello Github Actions! From debug build.");
+            Console.WriteLine("Hello Github Actions! From debug build.!");
         #endif
         #if RELEASE
-            Console.WriteLine("Hello Github Actions! From release build.!");
+            Console.WriteLine("Hello Github Actions! From release build.");
         #endif
     }
 }
