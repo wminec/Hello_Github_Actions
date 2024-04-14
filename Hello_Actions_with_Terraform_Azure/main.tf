b16e0be67f27809ec4cf84babc309c12b5c324da
wminec <wminec@gmail.com>
2024-04-14 17:59:15 +0900

    asd

--- Hello_Actions_with_Terraform_Azure/main.tf
+++ Hello_Actions_with_Terraform_Azure/main.tf
@@ -8,6 +8,13 @@ terraform {
     }
 
     required_version = ">= 1.1.0"
+
+    backend "azurerm" {
+        resource_group_name     = "backend-rg"
+        storage_account_name    = "sampletextsavk"
+        container_name          = "demosampletest"
+        key                     = "terraform.tfstate"
+    }
 }
 
 provider "azurerm" {
